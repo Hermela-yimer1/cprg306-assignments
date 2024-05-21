a92e154f1846aa5a2fda882683223a06dd12c914
Hermela-yimer1 <141375883+Hermela-yimer1@users.noreply.github.com>
2024-05-21 13:28:14 -0600

    Week-2
    Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+
+import Link from 'next/link';
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <p>Hermela Yimer</p>
+        <Link href="https://github.com/Hermela-yimer1">GitHub</Link> 
+      </main>
+    );
+  }
